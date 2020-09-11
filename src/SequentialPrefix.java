d6dc53f5805054b0337880fad22494575b5d0200
Chris Blackburn <chrisnblackburn@gmail.com>
2020-09-11 11:50:35 -0400

    Sequential done

--- src/SequentialPrefix.java
+++ src/SequentialPrefix.java
@@ -1,16 +1,34 @@
+import java.io.*;
+
 /**
  * Finds the prefix sum using a sequential algorithm and writes it to out,txt
  *
  */
 public class SequentialPrefix implements IPrefix {
-
     public void run(String inputFileName, int bufferSize) {
+        BufferedReader in;
+        BufferedWriter out;
+        String nStr;
+        int sum = 0;
 
+        try {
+            in = new BufferedReader(new FileReader(inputFileName));
+            out = new BufferedWriter(new FileWriter("out.txt"));
 
+            while (null != (nStr = in.readLine())) {
+                sum += Integer.parseInt(nStr);
+                out.write(String.valueOf(sum));
+                out.newLine();
+            }
+
+            in.close();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main (String [] args) {
         new SequentialPrefix().run("in.txt", 1000);
     }
-
 }
