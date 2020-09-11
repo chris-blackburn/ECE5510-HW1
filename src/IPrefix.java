04a8e7b384944469d97b1efe3f07525c7b9d44c1
Chris Blackburn <chrisnblackburn@gmail.com>
2020-09-11 11:51:09 -0400

    Internal parallel done

--- src/IPrefix.java
+++ src/IPrefix.java
@@ -1,3 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public interface IPrefix {
+    /* Gets a chunk of integers from a buffered reader */
+    static int getChunk(BufferedReader in, int chunk[]) throws IOException {
+        int i;
+        String nStr;
+
+        for (i = 0; i < chunk.length; i++) {
+            if (null == (nStr = in.readLine())) {
+                return i;
+            }
+
+            /* Assuming the input is clean... */
+            chunk[i] = Integer.parseInt(nStr);
+        }
+
+        return i;
+    }
+
     void run(String inputFileName, int bufferSize);
 }
--- src/ParallelPrefixInternal.java
+++ src/ParallelPrefixInternal.java
@@ -1,15 +1,46 @@
+import java.io.*;
+import java.util.Arrays;
+
 /**
  *  Uses the parallelPrefix function provided by Java to find the prefix sum
  *
  */
 public class ParallelPrefixInternal implements IPrefix {
-
     public void run(String inputFileName, int bufferSize) {
+        BufferedReader in;
+        BufferedWriter out;
+        String nStr;
+        int prevSum = 0;
 
+        int chunk[], iread;
+
+        try {
+            in = new BufferedReader(new FileReader(inputFileName));
+            out = new BufferedWriter(new FileWriter("out.txt"));
+
+            /* we have to read data in as chars and convert */
+            chunk = new int[bufferSize];
+
+            while (0 != (iread = IPrefix.getChunk(in, chunk))) {
+                Arrays.parallelPrefix(chunk, 0, iread, (n1, n2) -> n1 + n2);
+
+                for (int i = 0; i < iread; i++) {
+                    out.write(String.valueOf(chunk[i] + prevSum));
+                    out.newLine();
+                }
+
+                /* Save the sum for the next chunk */
+                prevSum += chunk[iread - 1];
+            }
+
+            in.close();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main (String [] args) {
         new ParallelPrefixInternal().run("in.txt", 1000);
     }
-
 }
